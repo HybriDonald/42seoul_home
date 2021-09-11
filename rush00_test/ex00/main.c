4cc3567de26ce77d4a7d756537ef032fa955499c
None <seongmin@Noneui-MacBookAir.local>
2021-09-11 13:57:09 +0900

    rush00 commit

---
+++ rush00_test/ex00/main.c
@@ -0,0 +1,8 @@
+void rush(int x, int y);	// Prototype of rush();
+
+int main() 
+{
+	rush(2, 2);
+	return (0);
+
+}
---
+++ rush00_test/ex00/rush04.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonghon <seonghon@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/11 12:34:04 by seonghon          #+#    #+#             */
+/*   Updated: 2021/09/11 13:51:14 by seonghon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_putchar( char c );	// Prototype of function ft_putchar( char c ); include ft_putchar.c
+
+void print_head( int x )	// Print top
+{
+	ft_putchar('A');
+
+	if(x != 1)
+	{
+		for(int i = 1; i < x-1; i++)
+		{
+			ft_putchar('B');
+		}
+		ft_putchar('C');
+	}
+
+	ft_putchar('\n');
+}
+
+void print_body( int x, int y)	// Print middle
+{
+	if(y != 1)
+	{
+		for(int j = 1; j < y - 1; j++)
+		{
+			ft_putchar('B');
+			for(int i = 1; i < x-1 ; i++)
+			{
+				ft_putchar(' ');
+			}
+
+			ft_putchar('B');
+			ft_putchar('\n');
+		}
+	}
+}
+
+void print_foot( int x )	// print bottom
+{
+	ft_putchar('C');
+
+	if(x != 1)
+	{
+		for(int i = 1; i < x-1; i++)
+		{
+			ft_putchar('B');
+		}
+		ft_putchar('C');
+	}
+
+	ft_putchar('\n');
+}
+
+void rush(x, y)
+{
+	if(x <= 0 || y <= 0)
+	{
+		return;
+	}
+
+	print_head(x);
+
+	if(y != 1)
+	{
+		print_body(x, y);
+		print_foot(x);
+	}
+}
