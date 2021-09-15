9fa9c0f48ab8b5617851472b434ff09b44e6f98e
Seongmin Hong <seonghon@c1r7s7.42seoul.kr>
2021-09-15 17:39:10 +0900

    add c_00

---
+++ c_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonghon <seonghon@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 16:35:02 by seonghon          #+#    #+#             */
+/*   Updated: 2021/09/15 17:16:54 by seonghon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
