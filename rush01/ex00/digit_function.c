dc0534576459d4759c33544239cf3cff4cfb18e0
None <seongmin@Noneui-MacBookAir.local>
2021-09-19 14:31:42 +0900

    add functions

--- rush01/ex00/digit_function.c
+++ rush01/ex00/digit_function.c
@@ -1,17 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush.h                                             :+:      :+:    :+:   */
+/*   digit_function.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seonghon <seonghon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/18 12:35:29 by seonghon          #+#    #+#             */
-/*   Updated: 2021/09/19 04:09:30 by seonghon         ###   ########.fr       */
+/*   Created: 2021/09/19 04:08:54 by seonghon          #+#    #+#             */
+/*   Updated: 2021/09/19 04:22:33 by seonghon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+int is_digit(char c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    return (0);
+}
 
-#include "string_function.c"
+int number_of_params(char *str)
+{
+    int i;
+    int count;
 
-#include "digit_function.c"
+    i = 0;
+    count = 0;
+    while (str[i])
+    {
+        if(is_digit(str[i]))
+        {
+            count++;
+        }
+        i++;
+    }
+    return (count);
+}
+
+int get_max_number(char *str)
+{
+    return (number_of_params(str) / 4);
+}
--- rush01/ex00/main.c
+++ rush01/ex00/main.c
@@ -6,7 +6,26 @@
 /*   By: seonghon <seonghon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 12:35:03 by seonghon          #+#    #+#             */
-/*   Updated: 2021/09/18 12:35:03 by seonghon         ###   ########.fr       */
+/*   Updated: 2021/09/19 14:22:50 by seonghon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "rush.h"
+
+int main(int argc, char **argv)
+{
+    int max;
+    
+    if(argc > 2)
+    {
+        ft_put_char("E");
+    }
+    max = 0;
+    max = get_max_number(argv[1]);
+    if(is_param_valid(argv[1], max))
+    {
+        ft_put_char("E");
+        ft_put_char("\n");
+    }
+    return (0);
+}
