5739c6a9429d5839495abc996946151909b1d4b4
Seongmin Hong <seonghon@c5r9s7.42seoul.kr>
2021-09-27 16:53:25 +0900

    added c02

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,16 +6,117 @@
 /*   By: seonghon <seonghon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 02:39:04 by seonghon          #+#    #+#             */
-/*   Updated: 2021/09/27 02:41:11 by seonghon         ###   ########.fr       */
+/*   Updated: 2021/09/27 16:48:07 by seonghon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_print_content(void *addr, unsigned int size)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < size)
+	{
+		c = *((char *)addr + i);
+		if (!(c >= 32 && c <= 126))
+			write(1, ".", 1);
+		else
+			write(1, &c, 1);
+		i++;
+	}
+}
+
+void	ft_print_hex(char *hex, int index, int i)
+{
+	while (--index >= 0)
+		write(1, &hex[index], 1);
+	if (i % 2)
+		write(1, " ", 1);
+}
+
+void	ft_print_content_hex(void *addr, unsigned int size)
+{
+	char	hex[2];
+	char	c;
+	int		i;
+	int		index;
+
+	i = 0;
+	while (i < size)
+	{
+		index = 0;
+		c = *((char *)addr + i);
+		while (index < 2)
+		{
+			hex[index] = c % 16;
+			if (hex[index] < 10)
+				hex[index] = hex[index] + '0';
+			else
+				hex[index] = (hex[index] - 10) + 'a';
+			c = c / 16;
+			index++;
+		}
+		ft_print_hex(hex, index, i);
+		i++;
+	}
+	if (size % 2)
+		write(1, " ", 1);
+}
+
+void	ft_print_addr(void *addr)
+{
+	char	hex[16];
+	long	addr_long;
+	int		i;
+
+	i = 0;
+	addr_long = (long)addr;
+	while (addr_long > 0)
+	{
+		hex[i] = addr_long % 16;
+		if (hex[i] < 10)
+			hex[i] += '0';
+		else
+			hex[i] = (hex[i] - 10) + 'a';
+		addr_long = addr_long / 16;
+		i++;
+	}
+	while (i < 16)
+	{
+		hex[i] = '0';
+		i++;
+	}
+	while (--i >= 0)
+		write(1, &hex[i], 1);
+}
+
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	int memory;
+	unsigned int	print_size;
+	unsigned int	index;
 
-	memory = &addr;
-
+	index = 0;
+	while (size > 0)
+	{
+		if (size >= 16)
+		{
+			print_size = 16;
+			size -= 16;
+		}
+		else
+		{
+			print_size = size;
+			size = 0;
+		}
+		ft_print_addr(addr + index);
+		write(1, ": ", 2);
+		ft_print_content_hex(addr + index, print_size);
+		ft_print_content(addr + index, print_size);
+		write(1, "\n", 1);
+		index += 16;
+	}
+	return (addr);
 }
