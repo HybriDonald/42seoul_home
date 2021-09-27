d75b4a15612edab777a915015c0d41805a88b1b8
None <seongmin@Noneui-MacBookAir.local>
2021-09-28 01:16:54 +0900

    modified c02/main.c

--- c02/main.c
+++ c02/main.c
@@ -1,8 +1,11 @@
 void	*ft_print_memory(void *addr, unsigned int size);
+void	ft_putstr_non_printable(char *str);
 
 int main(void)
 {
-    char    c[] = "jklmno*+,-12389fhsadf98h32./:;<=>?JKLMNOP\n\n\n\n\t\t\t\tsd";
-
-    ft_print_memory(c, sizeof(c));
+    char c[] = "sadflkj\n\n\n\n\n\t\t\t\t";
+    char d = (char)127;
+    ft_putstr_non_printable(c);
+    ft_putstr_non_printable(&d);
+    return (0);
 }
